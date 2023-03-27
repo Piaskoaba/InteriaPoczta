f28848d1e392f548859af5b9cd38317e1f891cbf
Piaskoba <bartosz.piaskowskii@gmail.com>
2023-03-27 14:20:52 +0200

    Uruchomienie testu

--- src/test/java/PocztaInteriaTest/Service.java
+++ src/test/java/PocztaInteriaTest/Service.java
@@ -6,13 +6,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class Service extends BasePage {
+public class Service {
 
+    WebDriver driver;
+    WebDriverWait webDriverWait;
 
     public Service(WebDriver driver) {
-        super(driver);
+        this.driver = driver;
+        webDriverWait = new WebDriverWait(driver, 15);
+        PageFactory.initElements(driver, this);
     }
 
     @FindBy(xpath = "/html/body/div[11]/div[2]/button[3]")
@@ -32,7 +38,7 @@ public class Service extends BasePage {
     }
 
     public String chromeDriverUrl() {
-        return "driver/chrome,driver";
+        return "webdriver.chrome.driver";
     }
 
     boolean isCookieButtonAbleToClick() {
@@ -42,10 +48,18 @@ public class Service extends BasePage {
         } catch (org.openqa.selenium.TimeoutException | org.openqa.selenium.NoSuchElementException e) {
             return false;
         }
-
     }
 
+    public String urlStringInteria() {
+        return "https://www.interia.pl";
+    }
+public String getDriver(){
+    return "C:/Program Files/DRIVERS/chromedriver.exe";
+
 }
+}
+
+
 
 
 
--- src/test/java/PocztaInteriaTest/TestJava.java
+++ src/test/java/PocztaInteriaTest/TestJava.java
@@ -1,13 +1,13 @@
 package PocztaInteriaTest;
 
 import PocztaInteriaPages.BasePage;
+import PocztaInteriaPages.HomePage;
+import PocztaInteriaPages.LoginPage;
 import org.junit.AfterClass;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
@@ -16,16 +16,28 @@ import java.util.Scanner;
 
 public class TestJava {
 
-@BeforeMethod
+    Service service;
+    WebDriver driver;
+
+
+    @BeforeMethod(alwaysRun = true)
+    public void runBrowser() {
+        driver = new ChromeDriver();
+        service = new Service(driver);
+        System.setProperty(service.chromeDriverUrl(),service.getDriver());
+        driver.get(service.urlStringInteria());
+        driver.manage().window().maximize();
+        }
+
 
     @Test
     public void demoTest() {
+        HomePage homePage = new HomePage(driver);
+        homePage.clickMailButton();
 
 
 
-
-
-       // String myLogin = "adam.testowyy@interia.pl";
+        // String myLogin = "adam.testowyy@interia.pl";
         //String MyPassword = "PocztaInteria123!";
 
     }
